24baa20ef5f83665ea84b51c813b0dacb393aba1
floris77 <florisvanderhart@gmail.com>
2025-03-24 01:00:40 +0100

    Add SwiftSoup package dependency

--- Package.swift
+++ Package.swift
@@ -1,1 +1,26 @@
+// swift-tools-version:5.5
+import PackageDescription
 
+let package = Package(
+    name: "DogTrainingNotifier",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "DogTrainingNotifier",
+            targets: ["DogTrainingNotifier"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.6.1"),
+    ],
+    targets: [
+        .target(
+            name: "DogTrainingNotifier",
+            dependencies: ["SwiftSoup"]),
+        .testTarget(
+            name: "DogTrainingNotifierTests",
+            dependencies: ["DogTrainingNotifier"]),
+    ]
+)
